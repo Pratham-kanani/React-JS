606d825613523f69b5c1da7cf7470e969890ff88
Pratham-kanani <p.kanani10a@gmail.com>
2025-04-19 21:28:14 +0530

    Understanding more about JSX and making custom/reactElement

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,13 @@
 import Chai from "./chai";
 
 function App() {
+  const username ='PRATHAM KANANI'
 
   return (
     <>
-    <h1>Chai aur react with vite | PRATHAM KANANI</h1>
+    <h1>Chai aur react with vite | {username}</h1>
     <Chai />
-    <p>lorem 10</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, quibusdam! Praesentium veritatis quam unde at, itaque repellendus laborum consectetur optio voluptates animi cumque natus? Sed dignissimos laboriosam facere libero distinctio?</p>
     </>
   )
 }
