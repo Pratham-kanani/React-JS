55c7e0bf247653f7954aca191d859f19030bdd90
Pratham-kanani <p.kanani10a@gmail.com>
2025-04-20 09:34:21 +0530

    Understanding more on counter React's interview prep

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -8,7 +8,24 @@ function App() {
 
   const addValue = () => {
     if(counter >= 0 && counter < 20){
-    setCounter(counter + 1)
+
+    //Even though there are four setCounter,
+    //it does not add value 4 because all of
+    //them updates the same previous counter
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+
+    //If we actually want to do that
+    //we need to put this in a call back function 
+    //expected by useState's setCounter
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+
+
     }else{
       setCounter(0)
     }
